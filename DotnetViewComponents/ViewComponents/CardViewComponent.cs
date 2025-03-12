8e472227d7bf73c8af440237771a131f64a311e6
akdalin <Dalin.Akrasi@nhs.net>
2025-03-12 16:13:11 +0000

    Document code

--- DotnetViewComponents/ViewComponents/CardViewComponent.cs
+++ DotnetViewComponents/ViewComponents/CardViewComponent.cs
@@ -3,8 +3,22 @@ namespace DotnetViewComponents.ViewComponents
     using Microsoft.AspNetCore.Mvc;
     using DotnetViewComponents.ViewModels;
 
+    /// <summary>
+    /// Represents a view component for rendering a card.
+    /// </summary>
     public class CardViewComponent : ViewComponent
     {
+        /// <summary>
+        /// Invokes the card view component with the specified parameters.
+        /// </summary>
+        /// <param name="title">The title of the card.</param>
+        /// <param name="description">The content description of the card. Optional.</param>
+        /// <param name="imageSrc">The source URL of the image to be displayed on the card. Optional.</param>
+        /// <param name="headingLevel">The HTML heading level for the title. Defaults to <c>2</c>, which results in h2.</param>
+        /// <param name="hasArrow">Indicates whether the card should display an arrow. Defaults to <c>false</c>.</param>
+        /// <param name="isSecondary">Indicates whether the card is styled as a secondary card. Defaults to <c>false</c>.</param>
+        /// <param name="isClickable">Indicates whether the card is clickable. Defaults to <c>true</c>.</param>
+        /// <returns>An <see cref="IViewComponentResult"/> that renders the card view.</returns>
         public IViewComponentResult Invoke(
             string title,
             string? description = null,
--- DotnetViewComponents/ViewModels/CardViewModel.cs
+++ DotnetViewComponents/ViewModels/CardViewModel.cs
@@ -2,6 +2,17 @@ namespace DotnetViewComponents.ViewModels
 {
     public class CardViewModel
     {
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CardViewModel"/> class.
+        /// </summary>
+        /// <param name="title">The title of the card.</param>
+        /// <param name="description">The content description of the card. Optional</param>
+        /// <param name="imageSrc">The source URL of the image to be displayed on the card. Optional.</param>
+        /// <param name="headingLevel">The HTML heading level for the title. Defaults to <c>2</c>, which results in h2.</param>
+        /// <param name="hasArrow">Indicates whether the card should display an arrow. Defaults to <c>false</c>.</param>
+        /// <param name="isSecondary">Indicates whether the card is styled as a secondary card. Defaults to <c>false</c>.</param>
+        /// <param name="isClickable">Indicates whether the card is clickable. Defaults to <c>true</c>.</param>
         public CardViewModel(
             string title,
             string? description = null,
