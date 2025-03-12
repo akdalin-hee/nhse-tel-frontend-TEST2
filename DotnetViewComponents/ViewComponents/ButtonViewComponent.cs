911621e08695a92367c834cb6d315ebc5e450d3a
akdalin <Dalin.Akrasi@nhs.net>
2025-03-12 16:04:30 +0000

    Document code

--- DotnetViewComponents/ViewComponents/ButtonViewComponent.cs
+++ DotnetViewComponents/ViewComponents/ButtonViewComponent.cs
@@ -3,8 +3,18 @@ namespace DotnetViewComponents.ViewComponents
     using Microsoft.AspNetCore.Mvc;
     using DotnetViewComponents.ViewModels;
 
+    /// <summary>
+    /// Represents a view component for rendering a button.
+    /// </summary>
     public class ButtonViewComponent : ViewComponent
     {
+        /// <summary>
+        /// Invokes the button view component with the specified parameters.
+        /// </summary>
+        /// <param name="text">The text to be displayed on the button.</param>
+        /// <param name="styling">The styling class for the button. Receives predefined styles from <see cref="ButtonStyle"/>. Defaults to <see cref="ButtonStyle.PRIMARY"/> if no input is passed to it.</param>
+        /// <param name="preventDoubleClick">Indicates whether to prevent double-clicking the button. Defaults to <c>false</c>.</param>
+        /// <returns>An <see cref="IViewComponentResult"/> that renders the button view.</returns>
         public IViewComponentResult Invoke(
             string text,
             string styling = ButtonStyle.PRIMARY,
--- DotnetViewComponents/ViewModels/ButtonViewModel.cs
+++ DotnetViewComponents/ViewModels/ButtonViewModel.cs
@@ -2,6 +2,12 @@ namespace DotnetViewComponents.ViewModels
 {
     public class ButtonViewModel
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ButtonViewModel"/> class.
+        /// </summary>
+        /// <param name="text">The text to be displayed on the button.</param>
+        /// <param name="styling">The styling class for the button. Defaults to <see cref="ButtonStyle.PRIMARY"/>.</param>
+        /// <param name="preventDoubleClick">Indicates whether to prevent double-clicking the button. Defaults to <c>false</c>.</param>
         public ButtonViewModel(
             string text,
             string styling = ButtonStyle.PRIMARY,
@@ -12,10 +18,19 @@ namespace DotnetViewComponents.ViewModels
             PreventDoubleClick = preventDoubleClick;
         }
 
+        /// <summary>
+        /// Gets or sets the text displayed on the button.
+        /// </summary>
         public string Text { get; set; }
 
+        /// <summary>
+        /// Gets or sets the styling for the button.
+        /// </summary>
         public string Styling { get; set; }
 
+        /// <summary>
+        /// Gets or sets a value indicating whether to prevent double-clicking the button.
+        /// </summary>
         public bool PreventDoubleClick { get; set; }
 
     }
