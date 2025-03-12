98ce9275a68e05b56e043a789cca58ae225717bb
akdalin <Dalin.Akrasi@nhs.net>
2025-03-12 15:39:34 +0000

    Document code

--- DotnetViewComponents/ViewModels/BreadcrumbsViewModel.cs
+++ DotnetViewComponents/ViewModels/BreadcrumbsViewModel.cs
@@ -2,6 +2,11 @@ namespace DotnetViewComponents.ViewModels
 {
     public class BreadcrumbsViewModel
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="BreadcrumbsViewModel"/> class.
+        /// It receives a list of of string tuples containing the name/title of the link and the url path to it.
+        /// </summary>
+        /// <param name="links">A list of tuples representing the breadcrumb links where each tuple contains a title and a URL.</param>
         public BreadcrumbsViewModel(List<(string Title, string Url)> links)
         {
             Links = links;
