4ec4d653fe93f55faa9cb89c2c27970b8540ffe4
akdalin-hee <124877763+akdalin-hee@users.noreply.github.com>
2025-07-03 15:52:37 +0100

    Update ButtonViewComponent.cs

--- DotnetViewComponents/ViewComponents/ButtonViewComponent.cs
+++ DotnetViewComponents/ViewComponents/ButtonViewComponent.cs
@@ -18,7 +18,7 @@ namespace DotnetViewComponents.ViewComponents
             bool preventDoubleClick = false)
         {
             var model = new ButtonViewModel(text, aspController, aspAction, aspRouteData, styling, style, preventDoubleClick);
-            var mo = 2;
+            var mo = 56;
 
             return View(model);
         }
