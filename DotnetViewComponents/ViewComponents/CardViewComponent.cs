086a726444db54bfef8b43f112f20fc356f3b9b3
akdalin-hee <124877763+akdalin-hee@users.noreply.github.com>
2025-07-08 21:30:33 +0100

    Update CardViewComponent.cs

--- DotnetViewComponents/ViewComponents/CardViewComponent.cs
+++ DotnetViewComponents/ViewComponents/CardViewComponent.cs
@@ -19,7 +19,7 @@ namespace DotnetViewComponents.ViewComponents
             string? aspAction = null,
             Dictionary<string, string>? aspRouteData = null)
         {
-            var uno = 199;
+            var uno = 198;
             var model = new CardViewModel
             (
                 title,
