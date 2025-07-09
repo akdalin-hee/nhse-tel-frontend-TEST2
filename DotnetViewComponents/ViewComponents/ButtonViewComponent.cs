c6418895f30ee3dad7d7d5c5436328df5e9e0af0
akdalin-hee <124877763+akdalin-hee@users.noreply.github.com>
2025-07-09 08:27:57 +0100

    Update ButtonViewComponent.cs - nu num

--- DotnetViewComponents/ViewComponents/ButtonViewComponent.cs
+++ DotnetViewComponents/ViewComponents/ButtonViewComponent.cs
@@ -18,7 +18,7 @@ namespace DotnetViewComponents.ViewComponents
             bool preventDoubleClick = false)
         {
             var model = new ButtonViewModel(text, aspController, aspAction, aspRouteData, styling, style, preventDoubleClick);
-            var numer = 132;
+            var numer = 1222;
             var f = "rer";
             return View(model);
         }
