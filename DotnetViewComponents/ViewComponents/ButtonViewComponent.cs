ea17872d4abf2ccfb495c86351bc90be4d84b38f
akdalin-hee <124877763+akdalin-hee@users.noreply.github.com>
2025-07-25 16:14:03 +0100

    fix: Update ButtonViewComponent.cs

--- DotnetViewComponents/ViewComponents/ButtonViewComponent.cs
+++ DotnetViewComponents/ViewComponents/ButtonViewComponent.cs
@@ -21,7 +21,7 @@ namespace DotnetViewComponents.ViewComponents
             var numer = 12225;
             var f = "rer";
             var di = "ciao44";
-            var anoth = "PATCH 15";
+            var anoth = "PATCH 156";
             return View(model);
         }
     }
